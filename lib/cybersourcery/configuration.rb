7f74e742168bfc8565362dba809e8ad2d962147d
Stephen Becker IV <github@deathbyescalator.com>
2016-07-15 16:22:57 -0700

    Erb config
    not my idea:
    https://github.com/dimko/cybersourcery/commit/e329d6ef16fd23e39c3e8c57dfd3c52e011c7abf

--- lib/cybersourcery/configuration.rb
+++ lib/cybersourcery/configuration.rb
@@ -3,7 +3,7 @@ module Cybersourcery
     attr_accessor :profiles_path, :sop_proxy_url, :sop_live_url, :sop_test_url
 
     def profiles
-      @profiles ||= YAML.load_file profiles_path
+      @profiles ||= YAML.load(ERB.new(File.read(profiles_path)).result)
     end
   end
 end
