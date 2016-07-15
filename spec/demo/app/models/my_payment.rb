f552f3d6497e84d3efa93dc3df8439a2bc69ced2
Stephen Becker IV <github@deathbyescalator.com>
2016-07-15 15:48:52 -0700

    remove demo there is a new project for this

--- spec/demo/app/models/my_payment.rb
+++
@@ -1,4 +0,0 @@
-class MyPayment < Cybersourcery::Payment
-  attr_accessor :bill_to_phone, :card_cvn, :card_expiry_dummy
-  validates_presence_of :card_cvn, :card_expiry_dummy
-end
--- spec/demo/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Demo::Application.routes.draw do
-  resources :carts, only: :new
-  root to: 'carts#new'
-  post 'pay', to: 'payments#pay'
-  post 'confirm', to: 'payments#confirm'
-end
