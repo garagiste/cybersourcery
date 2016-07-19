33b695528ac628bc7f556565d0c03d6229d818f2
Stephen Becker IV <github@deathbyescalator.com>
2016-07-19 11:13:24 -0700

    better match rails 5 return the params hash

--- lib/cybersourcery/cybersource_params_normalizer.rb
+++ lib/cybersourcery/cybersource_params_normalizer.rb
@@ -1,9 +1,10 @@
 module Cybersourcery
   class CybersourceParamsNormalizer
     def self.run(params)
-      params.keys.each do |key|
+      params.to_h.keys.each do |key|
         params[key[4..-1]] = params[key] if key =~ /^req_/
       end
+      params
     end
   end
 end
